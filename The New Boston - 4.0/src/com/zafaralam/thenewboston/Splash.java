d24fa5d354a91649deba43507064804411b2fa1f
zafar alam <fastalam420@gmail.com>
2012-06-26 23:15:14 +1000

    v0.1

---
+++ The New Boston - 4.0/src/com/zafaralam/thenewboston/Splash.java
@@ -0,0 +1,45 @@
+package com.zafaralam.thenewboston;
+
+import android.app.Activity;
+import android.content.Intent;
+//import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+	
+	//MediaPlayer ourSong;
+	
+	@Override
+	protected void onCreate(Bundle zafarActivity) {
+		// TODO Auto-generated method stub
+		super.onCreate(zafarActivity);
+		setContentView(R.layout.splash);
+		//ourSong = MediaPlayer.create(Splash.this, R.raw.start_song);
+		//ourSong.start();
+		Thread timer = new Thread(){
+			public void run() {
+				try {
+					sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openStartingPoint = new Intent("com.zafaralam.thenewboston.MENU");
+					startActivity(openStartingPoint);
+					
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		//ourSong.release();
+		finish();
+	}
+	
+	
+
+}
---
+++ The New Boston - 4.0/src/com/zafaralam/thenewboston/startingpoint.java
@@ -0,0 +1,48 @@
+package com.zafaralam.thenewboston;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class startingpoint extends Activity {
+    /** Called when the activity is first created. */
+	int counter;
+	Button add, sub;
+	TextView display;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setVariable();
+        
+        add.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//counter = counter + 1;
+				counter++;
+				display.setText("Your total is " + counter);
+			}
+		});
+        
+        sub.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter--;
+				display.setText("Your Total is " + counter);
+			}
+		});
+    }
+    
+    public void setVariable() {
+    	counter = 0;
+    	add = (Button) findViewById(R.id.bAdd);
+    	sub = (Button) findViewById(R.id.bSub);
+    	display = (TextView) findViewById(R.id.textView1);
+    }
+}
